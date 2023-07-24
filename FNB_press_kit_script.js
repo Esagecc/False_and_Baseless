f0c3a21e9c0c08c4377850ce588f0049c26c4450
Elizabeth Cantu <e.sage.cc@gmail.com>
2023-07-24 17:28:26 -0500

    complete navbar and header

---
+++ FNB_press_kit_script.js
@@ -0,0 +1,16 @@
+// Toggle Button
+const toggle = document.getElementById('toggle')
+const navbarShow = document.getElementById('nav')
+    document.onclick = function(e){
+        if(e.target.id !== 'navbarShow' && e.target.id !== 'toggle')
+        {
+            toggle.classList.remove('active')
+            navbarShow.classList.remove('active')
+        }
+    }
+
+    toggle.onclick = function(){
+        toggle.classList.toggle('active')
+        navbarShow.classList.toggle('active')
+    }
+
